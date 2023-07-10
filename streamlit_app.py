41f713fa22ccacc6951d9a437339809745ee8326
jayesh1397 <110031522+jayesh1397@users.noreply.github.com>
2023-07-10 21:46:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my families new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Pohe, Samosa')
+streamlit.text('Sabudana, chaha khari')
+streamlit.text('rawa, kachori')
