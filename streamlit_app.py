a1b0093ba5a729665857be377ace93e73d2aadbb
jayesh1397 <110031522+jayesh1397@users.noreply.github.com>
2023-07-10 21:49:11 +0530

    Update streamlit_app.py
    addded new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('my families new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Pohe, Samosa')
-streamlit.text('Sabudana, chaha khari')
-streamlit.text('rawa, kachori')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
