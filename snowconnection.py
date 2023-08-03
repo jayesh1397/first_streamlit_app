08d00a4a7a98bf41057483b305b52844bc3230c8
jayesh1397 <110031522+jayesh1397@users.noreply.github.com>
2023-08-03 15:45:57 +0530

    Create snowconnection.py
    snoflake connection test

---
+++ snowconnection.py
@@ -0,0 +1,13 @@
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
+
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
