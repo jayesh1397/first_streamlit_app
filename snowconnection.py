9089243fc8527f6e7a94f82491c927a339fd47da
jayesh1397 <110031522+jayesh1397@users.noreply.github.com>
2023-08-04 12:22:11 +0530

    Update snowconnection.py

--- snowconnection.py
+++ snowconnection.py
@@ -1,4 +1,6 @@
 import streamlit
 import pandas
-
+import snowflake.connector
 streamlit.header('Snowflake Healthcare App')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
