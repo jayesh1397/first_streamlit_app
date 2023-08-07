bf8631f7a56f9cf30f602446c7c9be218355395a
jayesh1397 <110031522+jayesh1397@users.noreply.github.com>
2023-08-07 12:02:05 +0530

    Update snowconnection.py

--- snowconnection.py
+++ snowconnection.py
@@ -3,6 +3,27 @@ import streamlit as st
 st.header('This is a header')
 st.subheader('This is a subheader')
 st.write('This is some text')
-#st.footer('This is a footer')
+st.footer('This is a footer')
+
+
+st.markdown(
+    """
+    <style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: red;
+        color: white;
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.write('This is some text')
+st.write('This is a footer')
 
  
