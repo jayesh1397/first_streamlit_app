993bbffc83470f0dab894a787610935c6b54d949
jayesh1397 <110031522+jayesh1397@users.noreply.github.com>
2023-08-07 11:59:27 +0530

    Update snowconnection.py

--- snowconnection.py
+++ snowconnection.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.header('This is a header')
 st.subheader('This is a subheader')
 st.write('This is some text')
-st.footer('This is a footer')
+#st.footer('This is a footer')
 
  
